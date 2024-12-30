1cf7f9a25ecc656ed2f57805c7ea9c71fe115f54
Zhalgas52 <241802@astanait.edu.kz>
2024-12-30 08:38:23 +0500

    Update Band.java

--- assignment 2/Band.java
+++ assignment 2/Band.java
@@ -1,1 +1,27 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+class Band extends Artist {
+    private int membersCount;
+
+    public Band(String name, String musicType, int membersCount) {
+        super(name, musicType);
+        this.membersCount = membersCount;
+    }
+
+    public int getMembersCount() {
+        return membersCount;
+    }
+
+    public void setMembersCount(int membersCount) {
+        this.membersCount = membersCount;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Members: " + membersCount;
+    }
+}
