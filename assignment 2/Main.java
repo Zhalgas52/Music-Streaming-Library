1ec85fd69d3e46abbc52c3d092f65729ea04a2ab
Zhalgas52 <241802@astanait.edu.kz>
2024-12-30 08:36:14 +0500

    Update Main.java

--- assignment 2/Main.java
+++ assignment 2/Main.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 class Main {
     public static void main(String[] args) {
         MusicManager manager = new MusicManager();
