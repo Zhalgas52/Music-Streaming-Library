49326682c8860d40a58bca53545f2be5ecc0a407
Zhalgas52 <241802@astanait.edu.kz>
2024-12-30 08:35:52 +0500

    Update Artist.java

--- assignment 2/Artist.java
+++ assignment 2/Artist.java
@@ -1,1 +1,50 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Artist {
+    private String name;
+    private String musicType;
+
+    public Artist(String name, String musicType) {
+        this.name = name;
+        this.musicType = musicType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMusicType() {
+        return musicType;
+    }
+
+    public void setMusicType(String musicType) {
+        this.musicType = musicType;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist: " + name + ", Genre: " + musicType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artist artist = (Artist) o;
+        return Objects.equals(name, artist.name) && Objects.equals(musicType, artist.musicType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, musicType);
+    }
+}
 
